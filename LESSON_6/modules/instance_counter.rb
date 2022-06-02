2594ac31b400907205c34c3671d9543e00b094b7
mkskovalev <maxonemore@ya.ru>
2022-06-02 14:01:42 +0300

    LESSON_6 fix 1

--- LESSON_6/modules/instance_counter.rb
+++ LESSON_6/modules/instance_counter.rb
@@ -12,6 +12,8 @@ module InstanceCounter
   end
 
   module InstanceMethods
+    private
+    
     def register_instance
       new = self.class.class_variable_get('@@instances') + 1
       self.class.class_variable_set('@@instances', new)
